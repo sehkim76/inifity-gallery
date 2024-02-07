c1b4bff7a493e9ce11d5b6ea9ea7dc1ba259c40f
sehkim76 <sehkim76@gmail.com>
2024-02-08 01:17:59 +0900

    Update ImageGallery.js

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -2,22 +2,10 @@ import React, { useState } from 'react'
 import '../App.css';
 
 const ImageGallery = ({imageUrls}) => {
-  const [selectedImage, setSelectedImage] = useState(null);
 
-  const handleImageClick = (imageUrl) => {
-    setSelectedImage(imageUrl);
-  };
-  console.log('ImageGallery', imageUrls);
   return (
     <div>
-    {selectedImage && (
-      <div className='modal-overlay'>
-        <div className='modal'>
-          <img src={selectedImage} alt='selected'/>
-          <button onClick={()=> setSelectedImage(null)}>닫기</button>
-        </div>
-      </div>      
-    )}
+
 
     </div>
   )
